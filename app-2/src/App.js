ae838272e84c9db228218c358d13a6944f501051
kaley forster <55896549+kale-redhead@users.noreply.github.com>
2021-05-19 10:19:22 -0400

    app-2

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -1,23 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import ListItem from './components/ListItem' 
 
 function App() {
+  const [list, nn] = useState([])
+
+  useEffect(() => {
+    axios.get('https://pokeapi.co/api/vs/pokemon').then((res) => {
+      setList(res.data.results)
+    })
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {list.map((element,index) => {
+        return <ListItem name={element.name} key={index}/>
+      })}
     </div>
   );
 }
